7f8998cd4be7f9b26a0bece6a9a2a7601457af65
Vera Lobova <pchilli@yandex.ru>
2022-10-21 15:38:24 +0300

    shift alt f2

--- TASK1 _VERSION3/Program.cs
+++ TASK1 _VERSION3/Program.cs
@@ -1,63 +1,4 @@
-﻿/*
-
-int[] EnterSomeNumbers(int i, int[] array)//вводим число, записываем в массивб если не число - завершаем.
-{
-
-
-    Console.WriteLine($"\nВведите целое число, если Вы закончили ввод, наберите 'Завершить' i = {i}");
-    string str = Console.ReadLine()!;
-    bool checkForEnd = int.TryParse(str, out int checkedNumber);//если число - попадаем в иф, если не число попадаем в элс
- Console.WriteLine("the array inside the method");//внутри метода массив записался
-    PrintArray(array);
-
-    if (checkForEnd)
-    {
-        checkedNumber = int.Parse(str);
-        Array.Resize(ref array, array.Length + 1);
-        array[i] = checkedNumber;
-       // int size = array.Length;
-        //Console.WriteLine($" \n arr i = {array[i]} arr length = {size} number {i} is {checkedNumber}");
-        i++;
-        /*Console.WriteLine("Print2");
-        PrintArray(array);
-       EnterSomeNumbers(i, array);//рекурсия - чтобы записывать числа, пока пользователю не надоест
-        
-    }
-    else
-    {
-        Console.WriteLine("Ввод данных завершен");
-    
-
-    }
-
-
-   return array;
-
-}
-
-
-
-void PrintArray(int[] arr)
-{
-    Console.Write($" \n Array:");
-    for (int i = 0; i < arr.Length; i++)
-    {
-
-        Console.Write($"\n {arr[i]} ");
-    }
-}
-
-
-void Main()
-{
-    int a = 0;
-    int[] array = new int[0];
-    int[] arrayOfYourNumbers = EnterSomeNumbers(a, array);
-     Console.Write($" \n the array outside the method:");//снаружи метода массив записал только первое значение
- PrintArray(arrayOfYourNumbers);
-
-}
-Main(); */
+﻿// нехороший метод решения задачи
 using System;
 class HelloWorld
 {
@@ -82,7 +23,7 @@ class HelloWorld
     {
         Console.WriteLine($"\nВведите целое число, если Вы закончили ввод, наберите 'Завершить'");
         string str = Console.ReadLine()!;
-        bool checkForEnd = int.TryParse(str, out int checkedNumber);//если число - попадаем в иф, если не число попадаем в элс
+        bool checkForEnd = int.TryParse(str, out int checkedNumber);//если число - попадаем в if, если не число попадаем в else
 
         if (checkForEnd)
         {
@@ -96,3 +37,4 @@ class HelloWorld
         }
     }
 }
+
